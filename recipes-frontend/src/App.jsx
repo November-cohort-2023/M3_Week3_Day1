11f456abf12d40a01c4227f579c6fec318f7698f
Omar <omaraakamal@gmail.com>
2024-03-26 23:42:09 +0400

    updated code

--- recipes-frontend/src/App.jsx
+++ recipes-frontend/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Route path='/' element={<RecipesPage/>}/>
       <Route path='/:recipeId' element={<RecipeDetails/>}/>
     </Routes>
-
     </>
   )
 }
--- recipes-frontend/src/pages/RecipeDetails.jsx
+++ recipes-frontend/src/pages/RecipeDetails.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import axios from 'axios'
 
 function RecipeDetails() {
+
+    const [recipe,setRecipe] = useState(null)
+
+    const {recipeId} = useParams()
+
+    useEffect(() => {
+      
+        axios.get(`http://localhost:5005/api/recipes/${recipeId}`)
+        .then((oneRecipe)=>{
+            console.log(oneRecipe.data)
+            setRecipe(oneRecipe.data)
+        })
+
+    }, [])
+    
+    console.log(recipeId)
   return (
-    <div>RecipeDetails</div>
+    <div>
+        {recipe && (
+            <div>
+                <h1>{recipe.title}</h1> 
+            </div>
+        )}
+      
+    </div>
   )
 }
 
