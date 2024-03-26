b0e8f57cd5a542dcbb74590f8a7503909b7a9fc8
Omar <omaraakamal@gmail.com>
2024-03-26 23:22:39 +0400

    updated code

--- recipes-frontend/src/pages/RecipesPage.jsx
+++ recipes-frontend/src/pages/RecipesPage.jsx
@@ -4,17 +4,29 @@ import axios from 'axios'
 
 function RecipesPage() {
 
+    const [recipes,setRecipes] = useState([])
+
     useEffect(() => {
       
         axios.get('http://localhost:5005/api/recipes/')
         .then((allRecipes)=>{
-            console.log(allRecipes)
+            setRecipes(allRecipes.data)
+            console.log(allRecipes.data)
         })
         
     }, [])
     
   return (
-    <div>RecipesPage</div>
+    <div>RecipesPage
+
+        {recipes.map((oneRecipe)=>{
+            return(
+                <div>
+                    <h1>{oneRecipe.title}</h1>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
