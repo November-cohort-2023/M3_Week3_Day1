e8a31111712e954569c22474095ee6928dbfd3d2
Omar <omaraakamal@gmail.com>
2024-03-26 22:35:24 +0400

    updated code

--- recipes-backend/routes/recipes.routes.js
+++ recipes-backend/routes/recipes.routes.js
@@ -1,7 +1,56 @@
+const Recipe = require('../models/Recipe.model')
 
 
 const router = require('express').Router()
 
+
+// CRUD
+
+router.get('/recipes',(req,res)=>{
+    
+    Recipe.find()
+    .then((allRecipes)=>{
+        console.log(allRecipes)
+        res.json(allRecipes)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.json(err)
+
+    })
+})
+
+router.get('/recipes/:id',(req,res)=>{
+
+    console.log(req.params)
+
+    Recipe.findById(req.params)
+    .then((foundRecipe)=>{
+        res.json(foundRecipe)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.json(err)
+
+    })
+
+})
+
+
+// Create for new recipe
+
+router.post('/recipes',(req,res)=>{
+    console.log(req.body)
+    Recipe.create(req.body)
+    .then((createdRecipe)=>{
+        res.json("recipe successfully created")
+    })
+    .catch(err=>{
+        console.log(err)
+        res.json(err)
+
+    })
+})
 
 
 
