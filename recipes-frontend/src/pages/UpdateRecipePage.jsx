19658f147d7cdd3c2ca35699aa54b1253670160f
Omar <omaraakamal@gmail.com>
2024-03-28 22:34:09 +0400

    updated

--- recipes-frontend/src/pages/UpdateRecipePage.jsx
+++ recipes-frontend/src/pages/UpdateRecipePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import axios from 'axios'
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function UpdateRecipePage() {
     
@@ -14,28 +14,48 @@ function UpdateRecipePage() {
     const [level,setLevel] = useState('Easy Peasy')
     const [chef,setChef] = useState('')
     const [error, setError] = useState()
+
+    const {recipeId} = useParams()
+
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
+    useEffect(() => {
+        axios.get(`http://localhost:5005/api/recipes/${recipeId}`)
+        .then((oneRecipe)=>{
+            console.log(oneRecipe.data)
+            setTitle(oneRecipe.data.title)
+            setChef(oneRecipe.data.chef)
+            setDuration(oneRecipe.data.duration)
+            setInstructions(oneRecipe.data.instructions)
+            setLevel(oneRecipe.data.level)
+
+        })  
+    }, [])
+    
   return (
     <div>
          <form onSubmit={handleSubmit}>
             <label>
                 Title:
-                <input minLength={1} required type="text" onChange={(e)=>{setTitle(e.target.value)}} />
+                <input value={title} minLength={1} required type="text" onChange={(e)=>{setTitle(e.target.value)}} />
             </label>
             <label>
                 Chef:
-                <input type="text" onChange={(e)=>{setChef(e.target.value)}} />
+                <input value={chef} type="text" onChange={(e)=>{setChef(e.target.value)}} />
             </label>
             <label>
                 Instructions:
-                <input type="text" onChange={(e)=>{setInstructions(e.target.value)}} />
+                <input value={instructions} type="text" onChange={(e)=>{setInstructions(e.target.value)}} />
             </label>
             <label>
                 Duration:
-                <input min={1} type="number" onChange={(e)=>{setDuration(e.target.value)}} />
+                <input value={duration} min={1} type="number" onChange={(e)=>{setDuration(e.target.value)}} />
             </label> 
        
 
-            <select onChange={(e)=>{setLevel(e.target.value)}}>
+            <select value={level} onChange={(e)=>{setLevel(e.target.value)}}>
                 <option value="Easy Peasy">Easy Peasy</option>
                 <option value="Amateur Chef">Amateur Chef</option>
                 <option value="UltraPro Chef">UltraPro Chef</option>
